2b068629c338cfd912d0c907683472caa1fb3d2d
ColtonZP <39172201+ColtonZP@users.noreply.github.com>
2020-04-13 14:03:21 -0700

    react app created

--- client/main.js
+++
@@ -1,5 +0,0 @@
-import React, {Component} from 'react';
-import {Meteor} from 'meteor/meteor';
-import {render} from 'react-dom';
-
-import '../imports/client/Routes';
--- imports/client/pages/NoMatch.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-NoMatch = () =>
-  <main>
-    <h1>No Match</h1>
-  </main>
-
-export default NoMatch;
