5d43586828aad0c41dcecd02f3bf57082a198354
ColtonZP <39172201+ColtonZP@users.noreply.github.com>
2020-04-23 16:53:59 -0700

    Updates

---
+++ precache-manifest.6333603b2099faf66d08b97a5bfb1c23.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3f6845445a56ad3cd14f463eaf83c3f",
+    "url": "/kemplet/index.html"
+  },
+  {
+    "revision": "722d645680c486537990",
+    "url": "/kemplet/static/css/main.2b6a4985.chunk.css"
+  },
+  {
+    "revision": "34bf828e0e49fe126469",
+    "url": "/kemplet/static/js/2.166a9d99.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kemplet/static/js/2.166a9d99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "722d645680c486537990",
+    "url": "/kemplet/static/js/main.d61280b8.chunk.js"
+  },
+  {
+    "revision": "de22df5c59b18a317da1",
+    "url": "/kemplet/static/js/runtime-main.3d0ecc41.js"
+  },
+  {
+    "revision": "a835792071db0264ef9b9c99242e51d3",
+    "url": "/kemplet/static/media/add.a8357920.svg"
+  }
+]);
--- src/functions/kemplet-date.js
+++
@@ -1,8 +0,0 @@
-exports.dateToday = (add) => {
-  const date = new Date();
-  let month = (date.getMonth() + 1).toString();
-  let day = date.getDate() + add;
-  const year = date.getFullYear();
-  const returnDate = [month, day, year].join("/");
-  return returnDate;
-};
