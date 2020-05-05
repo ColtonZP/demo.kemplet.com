f19cfe09d769431f3c3b1f405aeeb18a77c4738a
ColtonZP <czpemberton@gmail.com>
2020-05-05 15:46:56 -0700

    moved to statless

--- src/components/App.js
+++ src/components/App.js
@@ -1,127 +1,61 @@
 import React, { Component } from "react";
 import { quickId } from "quickids";
-import { Provider } from "mobx-react";
-import { dateToday } from "../functions/kemplet-date";
+import { inject, observer } from "mobx-react";
+import PropTypes from "prop-types";
 
-import TodoStore from "../stores/TodoStore";
 import Menu from "./Menu";
 import Today from "./Today";
 import TaskDetail from "./TaskDetail";
 
-export default class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      tasks: [
-        {
-          title: "Welcome",
-          due: dateToday(0),
-          id: "welcome",
-          complete: false,
-          todoLists: [
-            {
-              title: "Add a board",
-              id: "welcome-List-1",
-              completed: false,
-              todos: [
-                {
-                  todo:
-                    "to add a task board, click the add button at the top of the window.",
-                  completed: false,
-                },
-              ],
-            },
-            {
-              title: "Add a todo list",
-              id: "welcome-List-2",
-              completed: false,
-              todos: [
-                {
-                  todo:
-                    "add a list to the board by pressing the add button at the top of the board.",
-                  completed: false,
-                },
-              ],
-            },
-          ],
-        },
-      ],
-      dueToday: [],
-      openTask: {},
-    };
-  }
-
-  changeTaskId = (id) => {
-    this.setState({ viewTaskId: id });
-  };
-
-  addTask = (task) => {
-    const { tasks } = this.state;
-    task.id = quickId();
-    this.setState({
-      tasks: [...tasks, task],
-      openTask: task,
-    });
-  };
-
-  openTask = (task) => {
-    this.setState({
-      openTask: task,
-    });
-  };
-
-  removeTask = (id) => {
-    this.setState({
-      tasks: this.state.tasks.filter((task) => task.id !== id),
-      openTask: {},
-    });
-  };
-
-  componentDidMount() {
-    this.state.tasks[0] && this.openTask(this.state.tasks[0]);
-  }
-
-  render() {
-    const { tasks, openTask } = this.state;
-    let today = new Date();
+const App = inject("TodoStore")(
+  observer((props) => {
+    const today = new Date();
     today.setHours(0, 0, 0, 0);
+    const { TodoStore } = props;
+    const tasks = TodoStore.tasks;
+    // TodoStore.changeOpenTask("welcome");
     return (
-      <Provider TodoStore={TodoStore}>
-        <div className="App">
-          <Menu addTask={this.addTask} />
-          <div className="content">
-            <div className="tasks">
-              <div className="taskContent">
-                <div className="taskList">
-                  <Today tasks={tasks} />
-                  {tasks.map((task) => (
-                    <div
-                      key={task.id}
-                      className="card boardBtn"
-                      type="button"
-                      onClick={() => {
-                        this.openTask(task);
-                      }}
-                    >
-                      <div>{task.title}</div>
-                      {new Date(task.due).toDateString() ===
-                      today.toDateString() ? (
-                        <span className="dueToday">today</span>
-                      ) : (
-                        <span>{task.due.slice(0, -5)}</span>
-                      )}
-                    </div>
-                  ))}
-                </div>
-                {openTask.title && (
-                  <TaskDetail task={openTask} removeTask={this.removeTask} />
-                )}
+      <div className="App">
+        <Menu />
+        <div className="content">
+          <div className="tasks">
+            <div className="taskContent">
+              <div className="taskList">
+                {/* <Today tasks={tasks} /> */}
+                {/* {TodoStore.tasks.map((todo) => (
+                  <div className="card">{todo}</div>
+                ))} */}
+                {tasks.map((task) => (
+                  <div
+                    key={task.id}
+                    className="card boardBtn"
+                    type="button"
+                    onClick={() => {
+                      TodoStore.changeOpenTask(task.id);
+                    }}
+                  >
+                    <div>{task.title}</div>
+                    {new Date(task.due).toDateString() ===
+                    today.toDateString() ? (
+                      <span className="dueToday">today</span>
+                    ) : (
+                      <span>{task.due.slice(0, -5)}</span>
+                    )}
+                  </div>
+                ))}
               </div>
+              {TodoStore.openTask.id && <TaskDetail />}
             </div>
-            {/* <Calendar /> */}
           </div>
+          {/* <Calendar /> */}
         </div>
-      </Provider>
+      </div>
     );
-  }
-}
+  })
+);
+
+App.propTypes = {
+  TodoStore: PropTypes.func,
+};
+
+export default App;
--- src/components/TaskDetail.js
+++ src/components/TaskDetail.js
@@ -1,74 +1,46 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
+import { inject, observer } from "mobx-react";
 
 import TodoList from "./TodoList";
-import Progress from "./Progress";
-import Options from "./Options";
+// import Progress from "./Progress";
+// import Options from "./Options";
 
-class TaskDetail extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      title: "",
-    };
-  }
-
-  updateInput = (e) => {
-    this.setState({
-      title: e.target.value,
-    });
-  };
-
-  handleSubmit = (e) => {
-    e.preventDefault();
-    let adding = {
-      title: this.state.title,
-      todos: [],
-    };
-    this.props.task.todoLists.push(adding);
-    this.setState({
-      title: "",
-    });
-  };
-
-  render() {
-    const { task, removeTask } = this.props;
-
+const TaskDetail = inject("TodoStore")(
+  observer((props) => {
+    const task = props.TodoStore.openTask;
+    const removeTask = () => props.TodoStore.removeTask(task.id);
     return (
       <div className="taskDetailContainer card">
         {/* <Progress list={task.todoLists} /> */}
         <div className="taskDetails">
           {/* <Options id={task.id} removeTask={removeTask} /> */}
-          <h1 onClick={this.titleInput}>{task.title}</h1>
+          <h1>{task.title}</h1>
           <input
             type="button"
             value="remove"
             className="remove"
-            onClick={() => removeTask(task.id)}
+            onClick={removeTask}
           />
           <span>{`Due: ${task.due.slice(0, -5)}`}</span>
-          <form onSubmit={this.handleSubmit}>
-            <input
-              type="text"
-              value={this.state.title}
-              onChange={this.updateInput}
-              className="listText"
-            />
+          <form>
+            {/* onSubmit, add tasks */}
+            <input type="text" className="listText" />
             <input type="submit" value="add list" />
           </form>
           <div className="todoLists">
             {task.todoLists.map((list) => (
-              <TodoList list={list} key={list.title} />
+              <TodoList list={list} key={list.id} />
             ))}
           </div>
         </div>
       </div>
     );
-  }
-}
+  })
+);
 
 TaskDetail.propTypes = {
-  task: PropTypes.object,
+  TodoStore: PropTypes.func,
 };
 
 export default TaskDetail;
