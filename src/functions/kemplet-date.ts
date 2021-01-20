cd599e7fdb5a2208d56cd9aa88bd93ca7953df2a
Colton <czpemberton@gmail.com>
2021-01-20 15:22:02 -0800

    minor naming update

--- src/functions/kemplet-date.ts
+++ src/functions/kemplet-date.ts
@@ -1,7 +1,7 @@
-export function dateToday(add: number) {
+export function getDueDate(addDays: number) {
   const date = new Date();
   const month = (date.getMonth() + 1).toString();
-  const day = date.getDate() + add;
+  const day = date.getDate() + addDays;
   const year = date.getFullYear();
   const returnDate = [month, day, year].join('/');
   return returnDate;
